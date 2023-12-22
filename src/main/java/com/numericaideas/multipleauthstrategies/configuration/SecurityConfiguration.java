b25c10a09801695a6a1569573e8d9eff9d973cfa
kenany92 <captainnyobe@yahoo.fr>
2023-12-22 18:48:03 +0100

    Simple setup with disabled security

--- src/main/java/com/numericaideas/multipleauthstrategies/configuration/SecurityConfiguration.java
+++ src/main/java/com/numericaideas/multipleauthstrategies/configuration/SecurityConfiguration.java
@@ -1,4 +1,15 @@
 package com.numericaideas.multipleauthstrategies.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).build();
+    }
 }
--- src/main/java/com/numericaideas/multipleauthstrategies/controller/ApiController.java
+++ src/main/java/com/numericaideas/multipleauthstrategies/controller/ApiController.java
@@ -1,4 +1,15 @@
 package com.numericaideas.multipleauthstrategies.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ApiController {
+
+    @GetMapping
+    public String home() {
+        return "This is the response for the basic api call";
+    }
 }
